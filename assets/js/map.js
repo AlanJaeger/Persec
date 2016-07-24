89a728cd0f720b01508e6ea0ea2242fc19207541
Igor Fernandes <igorrfc10@gmail.com>
2016-07-24 18:24:47 -0300

    Modificações no css da página map, criado o form de contato e a sua lógica em javascript(arquivo map.js).

---
+++ assets/js/map.js
@@ -0,0 +1,7 @@
+$('#contact-us').on('click', function() {
+  $('#contact-form').fadeIn(700);
+});
+
+$('#contact-form #close-button').on('click', function() {
+  $('#contact-form').fadeOut(700);
+});
